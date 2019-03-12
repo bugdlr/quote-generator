259a619ae166ee6066f225b4ed7408c331667ce1
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-03-12 16:38:52 -0700

    quotes array and DOMstrings

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+const quotes;
+
+const DOMstrings = {
+  quoteBox: 'quote-box',
+  text: 'text',
+  author: 'author',
+  newQuoteBtn: 'new-quote',
+  tweetQuoteBtn: 'tweet-quote'
+}
+
 document.querySelector('h1').addEventListener('click', function(){
   console.log('h1 was clicked');
 })
+
+
+quotes = [
+  {text: "Begin to be now what you will be hereafter.",
+  author: "William James"},
+  {text: "Be miserable. Or motivate yourself. Whatever has to be done, it's always a choice.",
+  author: "Wayne Dyer"},
+  {text: "If you fell down yesterday, stand up today.",
+  author: "H.G. Wells"},
+  {text: "Always work hard. Intensity clarifies. It creates not only momentum, but also the pressure you need to feel either friction, or fulfillment.",
+  author: "Marcus Buckingham"}
+]
