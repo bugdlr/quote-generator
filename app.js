628c8631f9dde488532a2a73a74077fb83e06322
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-03-14 17:10:04 -0700

    random quotes and colors

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const quotes;
-
 const DOMstrings = {
   quoteBox: 'quote-box',
   text: 'text',
@@ -8,20 +6,46 @@ const DOMstrings = {
   tweetQuoteBtn: 'tweet-quote'
 }
 
-document.querySelector('h1').addEventListener('click', function(){
-  console.log('h1 was clicked');
-})
+class Quote {
+    constructor(text, author) {
+        this.text = text;
+        this.author = author;
+    }
+}
 
-
-quotes = [
-  {text: "Begin to be now what you will be hereafter.",
-  author: "William James"},
-  {text: "Be miserable. Or motivate yourself. Whatever has to be done, it's always a choice.",
-  author: "Wayne Dyer"},
-  {text: "If you fell down yesterday, stand up today.",
-  author: "H.G. Wells"},
-  {text: "Always work hard. Intensity clarifies. It creates not only momentum, but also the pressure you need to feel either friction, or fulfillment.",
-  author: "Marcus Buckingham"},
-  {text: "Challenges are what make life interesting and overcoming them is what makes life meaningful.",
-  author: "Joshua J. Marine"}
+const allQuotes = [
+  new Quote('"Begin to be now what you will be hereafter."', "William James"),
+  new Quote('"Be miserable. Or motivate yourself. Whatever has to be done, it\'s always a choice."', "Wayne Dyer"),
+  new Quote('"If you fell down yesterday, stand up today."', "H.G. Wells"),
+  new Quote('"Always work hard. Intensity clarifies. It creates not only momentum, but also the pressure you need to feel either friction, or fulfillment."', "Marcus Buckingham"),
+  new Quote('"Challenges are what make life interesting and overcoming them is what makes life meaningful."', "Joshua J. Marine")
 ]
+
+let random = (thing) => Math.floor(Math.random() * thing.length);
+
+function getNewQuote() {
+  let lastQuote, newQuote;
+  newQuote = allQuotes[random(allQuotes)];
+  if (newQuote !== lastQuote) {
+    document.getElementById("text").textContent = `${newQuote.text} ${newQuote.author}`;
+    getNewColors();
+  } else {
+    newQuote = allQuotes[random(allQuotes)];
+  }
+  lastQuote = newQuote;
+}
+
+
+// color generator
+const colors = ['#92B558', '#DC4C46', '#672e3b', '#c48f65', '#223a5e', '#898e8c', '#005960', '#9c9a40', '#4F84C4', '#578CA9', '#004b8d', '#edcdc2', '#ce3175', '#5a7247', '#cfb095', '#95dee3'];
+
+function getNewColors() {
+  let newColor = random(colors);
+  document.body.style.backgroundColor = colors[newColor];
+  document.getElementById('new-quote').style.backgroundColor = colors[newColor];
+  document.getElementById('tweet-quote').style.backgroundColor = colors[newColor];
+};
+
+getNewQuote();
+
+document.getElementById('new-quote').addEventListener('click', getNewQuote);
